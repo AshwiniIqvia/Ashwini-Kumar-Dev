85a4595dc514f10da8ce530ef0d871143b130620
AshwiniIqvia <99639168+AshwiniIqvia@users.noreply.github.com>
2022-03-03 16:09:47 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -96,10 +96,11 @@ define([
        
         payload['arguments'].execute.inArguments = [{
             
-            "adhoc": "{{Contact.Custom Activity.Test Active Data.AdhocText}}",
+            // "adhoc": "{{Contact.Custom Activity.Test Active Data.AdhocText}}",
             "studyId": "{{Contact.Custom Activity.Test Active Data.Clinical Trial Protocol ID}}",
-            "contactId": "{{Contact.Custom Activity.Test Active Data.Contact ID}}"
-            
+            "contactId": "{{Contact.Custom Activity.Test Active Data.Contact ID}}",
+            "adhoc": '{{adhoc.' + eventDefinitionKey + '.\"' + AdhocText + '\"}}'
+
            // 'serviceCloudId': '{{Event.' + eventDefinitionKey + '.\"' + idField + '\"}}'
 
             //"to": "{{Contact.Attribute.Test Custom Activity.TargetNumber}}" //<----This should map to your data extension name and phone number column
